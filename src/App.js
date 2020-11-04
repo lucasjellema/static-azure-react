b5705e684878f851592195814f39511711f5d4f6
Lucas Jellema <lucasjellema@gmail.com>
2020-11-04 16:26:56 +0100

    my own flavor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello Again - from React App {value}</div>;
 }
 
 export default App;
